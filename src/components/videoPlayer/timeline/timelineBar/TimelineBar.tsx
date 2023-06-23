ab2f77ec40e12fdafc49b2741137d1f0b0815cd5
alonzo245 <alon.alush.mail@gmail.com>
2023-06-23 13:14:02 +0300

    save

--- src/components/videoPlayer/timeline/timelineBar/TimelineBar.tsx
+++ src/components/videoPlayer/timeline/timelineBar/TimelineBar.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { usePlayerState } from "../../../../hooks/usePlayerState";
 import styles from "./timelineBar.module.css";
 
 export default () => {
+  const [position, setposition] = useState<string>("0px");
+  const { state, setState } = usePlayerState();
+  console.log(state?.currentVideo?.currentTime);
+
+  const niddlePosition = () => {
+    console.log("sss");
+
+    const timelineUnit = 480 / Number(state?.currentVideo?.duration);
+    return state?.currentVideo?.currentTime * timelineUnit;
+  };
+
+  useEffect(() => {
+    if (!state?.player?.isPlaying) {
+      setposition(`${niddlePosition()}px`);
+    }
+  }, [state?.player?.isPlaying]);
+
   return (
-    <div className={styles.container} style={{ left: "20px" }}>
+    <div className={styles.container} style={{ left: position }}>
       <div className={styles.niddle}></div>
     </div>
   );
