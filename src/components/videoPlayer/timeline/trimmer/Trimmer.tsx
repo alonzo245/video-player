bbf5c0b73fcde4f66b981598213e868cda1da726
alonzo245 <alon.alush.mail@gmail.com>
2023-06-23 12:49:32 +0300

    save

--- src/components/videoPlayer/timeline/trimmer/Trimmer.tsx
+++ src/components/videoPlayer/timeline/trimmer/Trimmer.tsx
@@ -2,7 +2,10 @@ import styles from "./trimmer.module.css";
 
 export default ({ type }: { type: "start" | "end" }) => {
   return (
-    <div className={styles.trimmer}>
+    <div
+      className={styles.trimmer}
+      style={type === "end" ? { right: "2px" } : { left: "2px" }}
+    >
       <div className={styles.handle} />
     </div>
   );
